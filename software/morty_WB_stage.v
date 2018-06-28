cc85b0077b8638ef27c8cc458d27ef568db82759
jesuscol96 <39978376+jesuscol96@users.noreply.github.com>
2018-06-27 23:09:43 -0400

    Update morty_WB_stage.v

--- software/morty_WB_stage.v
+++ software/morty_WB_stage.v
@@ -16,14 +16,15 @@ module WB_stage(
 				input wire [31:0] data_or_alu_i,  //data to WB.
 				input wire rf_we_wb_i, //Control signal
 				input wire [1:0] mux_sel_i,  //Control signal
-				input wire [1:0] csr_op_i,	//Control signal			
+				input wire [1:0] csr_op_i,	//Control signal
+				input wire comp_i, //control signal, used by WB for inst STL			
 				//interrupts ports
 				input wire        int_meip_wb_i,
            		input wire        int_mtip_wb_i,
            		input wire        int_msip_wb_i,
 				//Aditional CSR inputs
 				input wire is_csr_wb_i,   //Control signal
-				input wire is_rs1_wb_i,
+				input wire is_rs1_wb_i, //1 if rs1==0
 				//Outputs
 				output reg [31:0] writeback_data_o,
 				output reg [4:0] rd_wb_o,
@@ -40,7 +41,6 @@ module WB_stage(
 		assign is_interrupt = (int_meip_wb_i | int_msip_wb_i | int_mtip_wb_i);
 
 		always @(*) begin
-
 			//set signal for hazard unit
 			if(is_trap_wb_i | csr_err)
 				is_trap_wb_o=1'b1;  
@@ -48,16 +48,21 @@ module WB_stage(
 				is_trap_wb_o=1'b0;
 
 			//Select WB data
-			case(mux_sel_i)
-				2'b00: writeback_data_o=data_or_alu_i;
-				2'b01: writeback_data_o=PC4_wb_i;
-				2'b10: writeback_data_o=CSR_data_out;
-				2'b11: writeback_data_o=32'b0;
-			endcase	
+			if(is_trap_wb_i)
+				writeback_data_o=CSR_data_out;
+			else begin
+				
+				case(mux_sel_i)
+					2'b00: writeback_data_o=data_or_alu_i;
+					2'b01: writeback_data_o=PC4_wb_i;
+					2'b10: writeback_data_o=CSR_data_out;
+					2'b11: writeback_data_o={31'b0, comp_i};
+				endcase	
+			end
 
 			//Propagate WE to RF
 			rf_we_wb_o=rf_we_wb_i;
-			rd_wb_o=rd_wb_i;
+			rd_wb_o=rd_wb_i;		
 		end
 
 		//Implementing CSR
