60614a4acf44827d8f4256c0b57909b793e5534c
jesuscol96 <39978376+jesuscol96@users.noreply.github.com>
2018-06-27 23:05:57 -0400

    Update morty_MEM_stage.v
    Algunos cambios para la lectura/escritura de direcciones no alineadas

--- software/morty_MEM_stage.v
+++ software/morty_MEM_stage.v
@@ -1,4 +1,4 @@
-//MEM stage  
+//MEM stage  Jesus Colmenares / 14-11384
 
 `include "LSU.v"
 `include "load_unit.v"
@@ -15,13 +15,15 @@ module MEM_stage(   //inputs
 					input wire [11:0] csr_addr_mem_i,  //CSR ADDRESS                 (P)
 					input wire [3:0] trap_code_mem_i,  //EXCEPTION CODE             (P)
 					input wire is_trap_mem_i,  //TRAP FLAG                            (P)
+					input wire is_rs0_i,       //(P)
 					input wire we_mem_ctrl_i,  //Control Signal
 					input wire is_LS_mem_ctrl_i, //Control Signal
 					input wire [2:0] funct3_mem_ctrl_i,  //Control Signal
 					input wire data_or_alu_ctrl_i, //Control signal - 1 for MEM 0 for alu
 					input wire  wbm_ack_mem_i, //WB
 					input wire  wbm_err_mem_i, //WB
-					input wire [31:0] wbs_data_mem_i,  //WB
+					input wire [31:0] wbm_data_mem_i,  //WB
+					
 					
 
 					//Outputs
@@ -32,6 +34,7 @@ module MEM_stage(   //inputs
 					output reg [11:0] csr_addr_mem_o,    // (P)
 					output reg [3:0] trap_code_mem_o,    // (P)
 					output reg		  is_trap_mem_o,      // (P)
+					output reg 		  is_rs0_o,   // (P)
 					output reg [31:0] wbm_addr_mem_o,  //WB
                  	output reg [31:0] wbm_dat_mem_o,	//WB
                  	output reg [ 3:0] wbm_sel_mem_o,	//WB
@@ -54,9 +57,10 @@ module MEM_stage(   //inputs
 				csr_addr_mem_o = csr_addr_mem_i;  
 			    trap_code_mem_o = trap_code_mem_i;  
 				is_trap_mem_o = is_trap_mem_i;
+				is_rs0_o=is_rs0_i;
 
 				//Signals to memory
-				wbm_addr_mem_o= alu_out_mem_i;
+				wbm_addr_mem_o= {alu_out_mem_i[31:2], 2'b0};
 				wbm_dat_mem_o = rs2_data_mem_i;
 				wbm_we_mem_o = we_mem_ctrl_i;
 			
@@ -65,12 +69,13 @@ module MEM_stage(   //inputs
 
 		 wire [31:0] data_mem_chosen;  //Correct data from memory (LB,LH,LW...)
 
-		 LSU LSU_ins(is_LS_mem_ctrl_i, funct3_mem_ctrl_i, wbm_ack_mem_i, wbm_err_mem_i, wbm_sel_mem_o, wbm_cyc_mem_o, wbm_stb_mem_o, stall_mem_o);
+		 LSU LSU_ins(is_LS_mem_ctrl_i, funct3_mem_ctrl_i, alu_out_mem_i[1:0], wbm_ack_mem_i, wbm_err_mem_i, wbm_sel_mem_o, wbm_cyc_mem_o, wbm_stb_mem_o, stall_mem_o);
 
-		 load_unit load_unit_ins(wbs_data_mem_i, funct3_mem_ctrl_i, data_mem_chosen);
+		 load_unit load_unit_ins(wbm_data_mem_i, funct3_mem_ctrl_i, alu_out_mem_i[1:0], data_mem_chosen);
 
 		 assign data_or_alu_o = data_or_alu_ctrl_i ? data_mem_chosen : alu_out_mem_i ;  //MUX choose between alu result or data from memory
 
 	 endmodule
+
 
 		
