c9f6bb6d0a2be09c7e94a17f97f95d518f86674f
jesuscol96 <39978376+jesuscol96@users.noreply.github.com>
2018-07-03 20:52:11 -0400

    Update morty_LSU.v

--- software/morty_LSU.v
+++ software/morty_LSU.v
@@ -1,6 +1,7 @@
 //LSU  Author: Jesús Colmenares / 14-11384
 
-module LSU( input wire is_LS_i,
+module LSU( input wire clk_i,
+			input wire is_LS_i,
 			input wire [2:0] funct3_i,
 			input wire [1:0] addr_i,
 			//Wishbone signals
@@ -21,53 +22,64 @@ module LSU( input wire is_LS_i,
 	localparam SW=3'b010;
 
 
-	always @(*) begin
+	always @(posedge clk_i) begin
 
 		if(is_LS_i) begin
 
 				if (wbm_ack_i | wbm_err_i)  begin
 
-					wbm_stb_o=1'b0;
-					wbm_cyc_o=1'b0;				
-					stall_o=1'b0;
-					wbm_sel_o=4'b0;					
+					wbm_stb_o<=1'b0;
+					wbm_cyc_o<=1'b0;						
+					wbm_sel_o<=4'b0;					
 				end
 				else begin
-					wbm_stb_o=1'b1;
-					wbm_cyc_o=1'b1;				
-					stall_o=1'b1;
+					wbm_stb_o<=1'b1;
+					wbm_cyc_o<=1'b1;
+					
 
 					case(funct3_i)
 
 						SB: begin
 							case(addr_i)
-								2'b00: wbm_sel_o=4'b0001;
-								2'b01: wbm_sel_o=4'b0010;
-								2'b10: wbm_sel_o=4'b0100;
-								2'b11: wbm_sel_o=4'b1000;
+								2'b00: wbm_sel_o<=4'b0001;
+								2'b01: wbm_sel_o<=4'b0010;
+								2'b10: wbm_sel_o<=4'b0100;
+								2'b11: wbm_sel_o<=4'b1000;
 							endcase						
 						end
 						SH: begin
 							case(addr_i[0])
-								1'b0: wbm_sel_o=4'b0011;
-								1'b1: wbm_sel_o=4'b1100;								
+								1'b0: wbm_sel_o<=4'b0011;
+								1'b1: wbm_sel_o<=4'b1100;								
 							endcase							
 						end
-						SW: wbm_sel_o=4'b1111;
-						default: wbm_sel_o=4'b0;
+						SW: wbm_sel_o<=4'b1111;
+						default: wbm_sel_o<=4'b0;
 					endcase		
 				end
 		end
 		else begin
-			wbm_stb_o=1'b0;
-			wbm_cyc_o=1'b0;
-			wbm_sel_o=4'b0;			
-			stall_o=1'b0;
+			wbm_stb_o<=1'b0;
+			wbm_cyc_o<=1'b0;
+			wbm_sel_o<=4'b0;			
+			
 
 		end		
 	end
 
+	//Stall if LS instruction
+	always @(*) begin
+		if(is_LS_i & ~wbm_ack_i)
+			stall_o=1'b1;
+		else 
+			stall_o=1'b0;		
+		
+	end
+
 endmodule
+
+
+
 
 
             
