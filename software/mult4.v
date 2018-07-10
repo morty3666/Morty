bc7cf1527c5e94d2ce0eb4bb34442a6113db693f
jesuscol96 <39978376+jesuscol96@users.noreply.github.com>
2018-07-10 17:00:12 -0400

    Update mult4.v

--- software/mult4.v
+++ software/mult4.v
@@ -1,3 +1,4 @@
+
 // Modulo Multiplexor (verilog)
 // Ejemplo de multiplexor generico simple de 4 entradas
 
@@ -7,10 +8,10 @@ module mult4 (in1, in2, in3, in4, sel, out);
     output reg [31:0] out;
     always @(in1, in2, in3, in4, sel)
     case (sel)
-        0: out <= in1;
-        1: out <= in2;
-        2: out <= in3;
-        default: out <= in4;
+        0: out = in1;
+        1: out = in2;
+        2: out = in3;
+        default: out = in4;
 	endcase
-endmodule	
+endmodule		
 
