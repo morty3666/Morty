aa3ffc0164952dafab5d6f05ce265a408eafcee4
jesuscol96 <39978376+jesuscol96@users.noreply.github.com>
2018-07-10 16:27:29 -0400

    Update and rename morty_if_stage.v to if_stage.v

--- software/if_stage.v
+++ software/if_stage.v
@@ -1,13 +1,14 @@
 //IF stage XYZ processor
 
-module if_stage
-			#(	 parameter [31:0] RESET_ADDR = 32'h8000_0000) //PC at reset
-		     (	 input wire clk_i, //clock
+module if_stage( input wire	[31:0] RESET_ADDR,	
+		     	 input wire clk_i, //clock
 				 input wire rst_i,  //reset
+				 /* verilator lint_off UNUSED */
 				 input wire [31:0] next_PC_JB_i,  //PC jump or branch
 				 input wire [31:0] PC_trap_i, //PC if trap
 				 input wire [1:0]  next_PC_control_i, //Control signal
-				 input wire		   IF_en,  //IF enable	
+				 input wire		   IF_en,  //IF enable
+				 input wire        is_stallx_next,	
 
 				 output reg [31:0] instr_o,  //fetched instruction
 				 output reg [31:0] PC4_if_o, //PC+4
@@ -15,6 +16,7 @@ module if_stage
 				 output reg [3:0] trap_code_if_o,  //trap code
 				 output reg is_trap_if_o,     //flag for trap
 				 output reg  stall_if_o, //stall if in process.
+				 output reg  is_trap_taken,
 				 //Wishbone
 				 output reg [31:0] wbm_addr_if_o,                 
                  output reg        wbm_cyc_if_o,
@@ -33,22 +35,27 @@ module if_stage
 		wire PC_en;
 		reg [1:0] PC_control;
 		reg [31:0] PC_JB;
-		wire is_JB = PC_control ==2'b01;		
+		wire is_JB = PC_control ==2'b01;
+		reg is_stallx;
+
+		assign is_trap_taken = PC_control==2'b10;		
 
 		//Updating PC
 		always @(posedge clk_i) begin
 			if (rst_i) begin
 				pc <= RESET_ADDR;
 				PC_control <= 2'b0;
-				PC_JB <= 32'b0;				
+				PC_JB <= 32'b0;
+				is_stallx <= 1'b0;				
 			end
 			else  begin
-			  if((IF_en & PC_en) | PC_control==2'b01 | PC_control==2'b10) 
+			  if((IF_en & PC_en) | PC_control==2'b01 &  ~is_stallx  | PC_control==2'b10) 
 			  	pc <= next_pc;			  			
 			  else 
 			  	pc <= pc;
 			  PC_control <=next_PC_control_i;
-			  PC_JB <= next_PC_JB_i;			  			  
+			  PC_JB <= next_PC_JB_i;
+			  is_stallx <= is_stallx_next;			  			  
 
 			end
 			
@@ -60,7 +67,7 @@ module if_stage
 			case(PC_control)
 
 				2'b00: next_pc=pc+4;    //Just PC+4
-				2'b01: next_pc=PC_JB;  //If jump/branch
+				2'b01: next_pc={PC_JB[31:1], 1'b0};  //If jump/branch
 				2'b10: next_pc=PC_trap_i;  //If trap
 				default: next_pc=PC_trap_i;
 
@@ -72,7 +79,7 @@ module if_stage
 			instr_o=wbm_dat_if_i;  //Instruction is what comes from memory.
 
 			//Check if pc is aligned
-			if(pc[1:0]==2'b0)
+			if(next_pc[1:0]==2'b0)
 				misaligned_flag=1'b0;
 			else 
 				misaligned_flag=1'b1;
@@ -186,5 +193,6 @@ module if_stage
     end
 
    endmodule
+
 
 
